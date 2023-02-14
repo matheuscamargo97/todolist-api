6ff7907beb8032ddfba4bdf08630deff5e08f02d
Matheus Lopes <theus97.ml@gmail.com>
2023-02-14 14:55:24 -0300

    added: criação da api para criar/deletar/atualizar/listar uma TODO list

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const router = require('./router');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+
+module.exports = app;
+
